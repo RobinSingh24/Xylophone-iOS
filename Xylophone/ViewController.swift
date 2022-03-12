d7f9a5b75c30b4a8538db1e078b531e8b2339883
Robin SIngh <robin.122497@gmail.com>
2022-03-12 21:52:06 +0530

    added code for playing audio

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,53 @@
 //
 //  ViewController.swift
 //  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    @IBAction func cButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "C")
+    }
+    
+    @IBAction func dButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "D")
+    }
+    
+    @IBAction func eButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "E")
+    }
+    
+    @IBAction func fButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "F")
+    }
+    
+    @IBAction func gButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "G")
+    }
+    @IBAction func aButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "A")
+    }
+    @IBAction func bButtonPressed(_ sender: UIButton) {
+        playSound(_filename: "B")
+    }
+    
+    func playSound( _filename:String) {
+        
+        let url = Bundle.main.url(forResource: _filename, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        
+    }
+    
 }
 
